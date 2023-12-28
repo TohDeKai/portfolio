d906d1607125d42051e18aade694e5ac3f8c48c6
TohDeKai <tohdekai@outlook.com>
2023-12-28 23:55:37 +0800

    FEAT: styling nav

--- app/src/app/components/nav.tsx
+++ app/src/app/components/nav.tsx
@@ -16,7 +16,7 @@ export default function Nav({ menuItems }: NavProps) {
   return (
     <section>
       <div className="py-8 px-4 max-w-screen-xl lg:py-16 lg:px-12 md:flex">
-        <ul className="space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+        <ul className="space-y text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
           {menuItems.map((menuItem, index) => (
             <li key={index} className="group">
               <a
@@ -24,7 +24,7 @@ export default function Nav({ menuItems }: NavProps) {
                 className="inline-flex items-center py-3 rounded-lg"
               >
                 <hr className="w-10 h-0.5 bg-gray-500 border-0 rounded group-hover:w-40 transition-all duration-300 group-hover:bg-gray-50" />
-                <p className="pl-8 text-sm text-gray-500 group-hover:text-gray-50">
+                <p className="pl-8 text-sm text-gray-500 group-hover:text-gray-50 uppercase">
                   {menuItem.text}
                 </p>
               </a>
