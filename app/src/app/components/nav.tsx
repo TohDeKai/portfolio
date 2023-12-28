1c1e29dcd7f755665265bf5d76246276b3cf5545
TohDeKai <tohdekai@outlook.com>
2023-12-28 23:54:19 +0800

    FEAT: added nav

---
+++ app/src/app/components/nav.tsx
@@ -0,0 +1,37 @@
+// Nav.tsx
+
+import React from "react";
+
+// Define the type for the menu item
+type MenuItem = {
+  text: string;
+  link: string;
+};
+
+type NavProps = {
+  menuItems: MenuItem[];
+};
+
+export default function Nav({ menuItems }: NavProps) {
+  return (
+    <section>
+      <div className="py-8 px-4 max-w-screen-xl lg:py-16 lg:px-12 md:flex">
+        <ul className="space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+          {menuItems.map((menuItem, index) => (
+            <li key={index} className="group">
+              <a
+                href={menuItem.link}
+                className="inline-flex items-center py-3 rounded-lg"
+              >
+                <hr className="w-10 h-0.5 bg-gray-500 border-0 rounded group-hover:w-40 transition-all duration-300 group-hover:bg-gray-50" />
+                <p className="pl-8 text-sm text-gray-500 group-hover:text-gray-50">
+                  {menuItem.text}
+                </p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
--- app/src/app/components/self.tsx
+++ app/src/app/components/self.tsx
@@ -1,7 +1,7 @@
 export default function Self() {
     return (
         <section>
-            <div className="py-8 px-4 max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <div className="pt-8 px-4 max-w-screen-xl text-center lg:pt-16 lg:px-12">
                 <h1 className="mb-4 text-left text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Toh De Kai</h1>
                 <h2 className="mb-4 text-left text-2xl font-medium tracking-normal leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Aspiring Software Engineer</h2>
                 <p className="mb-8 text-left text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Junior at the National University of Singapore</p>
