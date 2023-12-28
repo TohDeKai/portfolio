ed14f9c446c7a979a50506eb52771b3deb64aa1e
TohDeKai <tohdekai@outlook.com>
2023-12-29 00:07:02 +0800

    FEAT: responsiveness

--- app/src/app/components/self.tsx
+++ app/src/app/components/self.tsx
@@ -1,11 +1,14 @@
 export default function Self() {
     return (
-        <section>
-            <div className="pt-8 px-4 max-w-screen-xl text-center lg:pt-16 lg:px-12">
-                <h1 className="mb-4 text-left text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Toh De Kai</h1>
-                <h2 className="mb-4 text-left text-2xl font-medium tracking-normal leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Aspiring Software Engineer</h2>
-                <p className="mb-8 text-left text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Junior at the National University of Singapore</p>
-            </div>
-        </section>
+      <section>
+        <div className="pt-8 px-4 max-w-screen-xl text-center lg:pt-16 lg:px-12 2xl:ml-72 flex flex-col lg:flex-row items-start lg:items-center">
+          <div className="lg:w-1/2 lg:pr-8 flex-shrink-0 w-full lg:w-auto pr-0 lg:pr-4">
+            <h1 className="mb-4 text-left text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Toh De Kai</h1>
+            <h2 className="mb-4 text-left text-2xl font-medium tracking-normal leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Aspiring Software Engineer</h2>
+            <p className="mb-8 text-left text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Junior at the National University of Singapore</p>
+          </div>
+        </div>
+      </section>
     );
-}
+  }
+  
