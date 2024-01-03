d29e669ce6e0b6052ddc86db8c165759eaa30cf2
TohDeKai <tohdekai@outlook.com>
2024-01-03 17:32:22 +0800

    MISC: add misc data

--- app/src/app/components/misc.tsx
+++ app/src/app/components/misc.tsx
@@ -1,27 +1,41 @@
+import miscData from "../../../../app/public/data/misc.json";
+
+interface MiscData {
+  title: string;
+  date: string;
+  description: string;
+}
+
 export default function Misc() {
   return (
     <section>
-      <div className="px-4 max-w-screen-xl py-8 lg:px-12 2xl:mr-54 flex flex-col lg:flex-row items-start lg:items-center">
-        <a
-          href="#"
-          className="rounded-lg shadow md:flex-row w-full bg-slate-900 hover:bg-slate-700"
-        >
-          <div className="flex justify-between leading-normal px-12 py-4">
-            <div className="flex gap-6 mb-2">
-              <p className="text-lg font-bold text-gray-50 my-auto">
-                Award/Certification
-              </p>
+      <div className="px-4 max-w-screen-xl py-8 lg:px-12 2xl:mr-54 flex flex-col items-start lg:items-center">
+        {miscData.map((miscItem: MiscData, index: number) => (
+          <a
+            key={index}
+            className="rounded-lg shadow md:flex-row w-full bg-slate-900 hover:bg-slate-700"
+          >
+            <div className="flex justify-between leading-normal px-12 py-2">
+              <div className="flex gap-6 ">
+                <p className="text-sm font-bold text-gray-50 my-auto">
+                  {miscItem.title}
+                </p>
+              </div>
+              <h5 className="text-xs font-thin tracking-tight text-gray-400">
+                {miscItem.date}
+              </h5>
             </div>
-            <h5 className="text-sm font-thin tracking-tight text-gray-400">
-              MMM YYYY
-            </h5>
-          </div>
-          <div className="mb-3 px-12 font-normal text-md text-gray-400">
-            <p className="mb-3 font-normal text-gray-400">
-              Project Description
-            </p>
-          </div>
-        </a>
+            {miscItem.description.trim() !== "" && (
+              <div className="mb-2 px-12 font-extralight text-xs text-gray-400">
+                <ul className="list-disc list-inside">
+                  <li className="pl-4 font-normal text-gray-400">
+                    {miscItem.description}
+                  </li>
+                </ul>
+              </div>
+            )}
+          </a>
+        ))}
       </div>
     </section>
   );
