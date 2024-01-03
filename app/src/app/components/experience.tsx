466a96ef0e8131dccf35c84678a0b7131c3172c7
TohDeKai <tohdekai@outlook.com>
2024-01-03 16:57:59 +0800

    FEAT: add experience data

--- app/src/app/components/experience.tsx
+++ app/src/app/components/experience.tsx
@@ -1,59 +1,56 @@
+import React from "react";
+import experienceData from "../../../../app/public/data/experience.json";
+
+interface ExperienceData {
+  company: string;
+  role: string;
+  duration: string;
+  points: string[];
+  skills: string[];
+}
+
 export default function Experience() {
   return (
     <section>
-      <div className="px-4 max-w-screen-xl py-8 lg:px-12 2xl:mr-54 flex flex-col lg:flex-row items-start lg:items-center">
-        <a
-          href="#"
-          className="rounded-lg shadow md:flex-row w-full bg-slate-900 hover:bg-slate-700"
-        >
-          <div className="flex justify-between leading-normal px-12 py-4">
-            <div className="flex gap-6 mb-2">
-              <p className="text-lg font-bold text-gray-50 my-auto">Company</p>
-              <p className="text-md font-light text-gray-400 my-auto">Role</p>
+      <div className="px-4 max-w-screen-xl py-8 lg:px-12 2xl:mr-54 flex flex-col  items-start lg:items-center">
+        {experienceData.map((experience: ExperienceData, index: number) => (
+          <a
+            key={index}
+            href="#"
+            className="rounded-lg shadow md:flex-row w-full bg-slate-900 hover:bg-slate-700"
+          >
+            <div className="flex justify-between leading-normal px-12 py-4">
+              <div className="flex gap-4">
+                <p className="text-lg font-bold text-gray-50 my-auto">
+                  {experience.company}
+                </p>
+                <p className="text-sm font-light text-gray-400 my-auto">
+                  {experience.role}
+                </p>
+              </div>
+              <h5 className="text-xs font-thin tracking-tight text-gray-400 my-auto">
+                {experience.duration}
+              </h5>
             </div>
-            <h5 className="text-sm font-thin tracking-tight text-gray-400">
-              MMM YYYY - MMM YYYY
-            </h5>
-          </div>
-          <div className="mb-3 px-12 font-normal text-md text-gray-400">
-            <ul className="list-disc list-inside">
-              <li>Point #1</li>
-              <li>Point #1</li>
-              <li>Point #1</li>
-              <li>Point #1</li>
-              <li>Point #1</li>
-            </ul>
-          </div>
-          <div className="flex flex-wrap px-12 pb-4">
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg">
-              Skills
-            </span>
-          </div>
-        </a>
+            <div className="mb-3 px-12 font-normal text-sm text-gray-400">
+              <ul className="list-disc list-inside space-y-2">
+                {experience.points.map((point, pointIndex) => (
+                  <li key={pointIndex}>{point}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex flex-wrap px-12 pb-4">
+              {experience.skills.map((skill, skillIndex) => (
+                <span
+                  key={skillIndex}
+                  className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-4 py-1 my-2 rounded-lg"
+                >
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
