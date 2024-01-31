c8afb99527ef28d96cb5953315a2558bbf181066
TohDeKai <tohdekai@outlook.com>
2024-01-31 14:11:34 +0800

    FIX: fix padding and right justify self component

--- app/src/app/components/self.tsx
+++ app/src/app/components/self.tsx
@@ -1,18 +1,16 @@
 export default function Self() {
   return (
-    <section>
-      <div className="pt-8 px-4 max-w-screen-xl text-center lg:pt-16 lg:px-12 2xl:ml-72 flex flex-col lg:flex-row items-start lg:items-center">
-        <div className="flex-shrink-0 w-full px-4 pt-8 lg:px-12 lg:w-auto pr-0 lg:pr-4">
-          <h1 className="mb-4 text-left text-4xl font-extrabold tracking-tight leading-none text-blue-700 md:text-5xl lg:text-6xl ">
-            Toh De Kai
-          </h1>
-          <h2 className="mb-4 text-left text-2xl font-medium tracking-normal leading-none text-white md:text-3xl lg:text-4xl ">
-            Aspiring Software Engineer
-          </h2>
-          <p className="lg:mb-8 text-left text-lg font-normal text-slate-400 lg:text-xl ">
-            Junior at the National University of Singapore
-          </p>
-        </div>
+    <section className="flex justify-end">
+      <div className="w-full px-8 pt-16 py-4 lg:pt-32 flex flex-col">
+        <h1 className="mb-4 lg:text-right text-4xl font-extrabold tracking-tight leading-none text-blue-700 md:text-5xl lg:text-6xl">
+          Toh De Kai
+        </h1>
+        <h2 className="mb-4 lg:text-right text-2xl font-medium tracking-normal leading-none text-white md:text-3xl lg:text-4xl">
+          Aspiring Software Engineer
+        </h2>
+        <p className="lg:mb-8 lg:text-right text-lg font-normal text-slate-400 lg:text-xl">
+          Junior at the National University of Singapore
+        </p>
       </div>
     </section>
   );
