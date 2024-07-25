8151b58cbead3636ddb5272b98d9f6c846651f01
TohDeKai <tohdekai@outlook.com>
2024-07-25 17:20:34 +0800

    fix: fix layout

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -16,18 +16,22 @@ const menuItems = [
 export default function Home() {
   return (
     <main className="selection:bg-cyan-400 selection:text-slate-700">
-      <div className=" bg-slate-950 w-full lg:fixed lg:w-1/2 lg:h-full lg:top-0 lg:left-0 ">
-        <Self />
-        <SocMed />
-        <div className="hidden lg:block">
-          <Nav menuItems={menuItems} />
+      <div className="bg-slate-950 w-full lg:fixed lg:w-1/2 lg:h-full lg:top-0 lg:left-0 flex lg:items-start lg:justify-end">
+        <div className="p-4">
+          <Self />
+          <SocMed />
+          <div className="hidden lg:block">
+            <Nav menuItems={menuItems} />
+          </div>
         </div>
       </div>
-      <div className="bg-slate-950 w-full lg:relative h-full lg:w-1/2 lg:top-0 lg:left-1/2  ">
-        <About />
-        <Experience />
-        <Projects />
-        <Misc />
+      <div className="bg-slate-950 w-full lg:relative h-full lg:w-1/2 lg:top-0 lg:left-1/2 mr-24">
+        <div className=" w-full 2xl:w-1/2 pr-4">
+          <About />
+          <Experience />
+          <Projects />
+          <Misc />
+        </div>
       </div>
     </main>
   );
