93cd3978737f4e0b0b66dc21d756b294811198ba
TohDeKai <tohdekai@outlook.com>
2024-11-24 21:07:33 +0800

    fix: fix font sizes

--- app/src/app/components/footer.tsx
+++ app/src/app/components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <section id="about">
-      <div className="w-full px-8 py-4 pb-8 pt-16 flex flex-col ">
-        <p className="text-left font-normal text-gray-500 dark:text-gray-400 ">
+      <div className="w-full px-8 xl:px-4 py-8">
+        <p className="text-left text-sm font-normal text-gray-500 dark:text-gray-400 ">
           Built with{" "}
           <a href="https://nextjs.org/" target="_blank">
             Next.js
